a71af46d1ba54ad025ddee196b287de2847b0a43
Li Jie <cpunion@gmail.com>
2025-01-02 11:15:12 +0800

    support escaped chars (doesn't processed in actson)

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
 mod error;
+mod json;
 mod params;
 pub mod query_parser;
 mod serde;
-#[cfg(test)]
-mod tests;
 mod traits;
 mod upload_file;
 mod value;
 
 pub use error::*;
+pub use json::*;
 pub use params::*;
-pub use query_parser::*;
 pub use serde::*;
 pub use traits::*;
 pub use upload_file::*;
